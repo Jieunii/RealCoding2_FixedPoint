8e0868db9ab6a15ab0b661d243473d816c584b60
김영진 <youngjin033@ajou.ac.kr>
2020-08-24 03:54:35 +0900

    Add fx_ajou project main code

---
+++ fx_ajou.h
@@ -0,0 +1,26 @@
+#include "headerfile/fx_s1615_double.h"
+#include "headerfile/fx_s3231_double.h"
+#include "headerfile/fx_s1516_double.h"
+#include "headerfile/fx_s3132_double.h"
+#include "headerfile/fx_s2308_double.h"
+#include "headerfile/fx_s4716_double.h"
+
+#include "headerfile/fx_s1615_longlong.h"
+#include "headerfile/fx_s3231_longlong.h"
+#include "headerfile/fx_s1516_longlong.h"
+#include "headerfile/fx_s3132_longlong.h"
+#include "headerfile/fx_s2308_longlong.h"
+#include "headerfile/fx_s4716_longlong.h"
+
+#define _FX_DOUBLE 1
+#define _FX_LONGLONG 2
+
+#define _FX_S1615 1
+#define _FX_S3231 2
+#define _FX_S1516 3
+#define _FX_S3132 4
+#define _FX_S2308 5 
+#define _FX_S4716 6
+
+
+ 
